a96b15c43d5fe3803339f8874a3911ab871ef3cf
Mike Assel <mike.assel@pexip.com>
2025-09-02 21:18:53 -0400

    fix: update SystemLocationCreateRequest fields to remove omitempty for all non-required fields

--- config/system_location_model.go
+++ config/system_location_model.go
@@ -79,35 +79,35 @@ type SystemLocationCreateRequest struct {
 // SystemLocationUpdateRequest represents a request to update a system location
 type SystemLocationUpdateRequest struct {
 	Name                        string   `json:"name,omitempty"`
-	Description                 string   `json:"description,omitempty"`
-	MTU                         int      `json:"mtu,omitempty"`
-	MediaQoS                    *int     `json:"media_qos,omitempty"`
-	SignallingQoS               *int     `json:"signalling_qos,omitempty"`
-	LocalMSSIPDomain            string   `json:"local_mssip_domain,omitempty"`
-	DNSServers                  []string `json:"dns_servers,omitempty"`
-	NTPServers                  []string `json:"ntp_servers,omitempty"`
-	SyslogServers               []string `json:"syslog_servers,omitempty"`
-	H323Gatekeeper              *string  `json:"h323_gatekeeper,omitempty"`
-	SIPProxy                    *string  `json:"sip_proxy,omitempty"`
-	MSSIPProxy                  *string  `json:"mssip_proxy,omitempty"`
-	TeamsProxy                  *string  `json:"teams_proxy,omitempty"`
-	OverflowLocation1           *string  `json:"overflow_location1,omitempty"`
-	OverflowLocation2           *string  `json:"overflow_location2,omitempty"`
-	TranscodingLocation         *string  `json:"transcoding_location,omitempty"`
-	BDPMPinChecksEnabled        string   `json:"bdpm_pin_checks_enabled,omitempty"`
-	BDPMScanQuarantineEnabled   string   `json:"bdpm_scan_quarantine_enabled,omitempty"`
-	UseRelayCandidatesOnly      bool     `json:"use_relay_candidates_only,omitempty"`
-	SNMPNetworkManagementSystem *string  `json:"snmp_network_management_system,omitempty"`
-	HTTPProxy                   *string  `json:"http_proxy,omitempty"`
-	TURNServer                  *string  `json:"turn_server,omitempty"`
-	STUNServer                  *string  `json:"stun_server,omitempty"`
-	ClientTURNServers           []string `json:"client_turn_servers,omitempty"`
-	ClientSTUNServers           []string `json:"client_stun_servers,omitempty"`
-	PolicyServer                *string  `json:"policy_server,omitempty"`
-	EventSinks                  []string `json:"event_sinks,omitempty"`
-	LiveCaptionsDialOut1        *string  `json:"live_captions_dial_out_1,omitempty"`
-	LiveCaptionsDialOut2        *string  `json:"live_captions_dial_out_2,omitempty"`
-	LiveCaptionsDialOut3        *string  `json:"live_captions_dial_out_3,omitempty"`
+	Description                 string   `json:"description"`
+	MTU                         int      `json:"mtu"`
+	MediaQoS                    *int     `json:"media_qos"`
+	SignallingQoS               *int     `json:"signalling_qos"`
+	LocalMSSIPDomain            string   `json:"local_mssip_domain"`
+	DNSServers                  []string `json:"dns_servers"`
+	NTPServers                  []string `json:"ntp_servers"`
+	SyslogServers               []string `json:"syslog_servers"`
+	H323Gatekeeper              *string  `json:"h323_gatekeeper"`
+	SIPProxy                    *string  `json:"sip_proxy"`
+	MSSIPProxy                  *string  `json:"mssip_proxy"`
+	TeamsProxy                  *string  `json:"teams_proxy"`
+	OverflowLocation1           *string  `json:"overflow_location1"`
+	OverflowLocation2           *string  `json:"overflow_location2"`
+	TranscodingLocation         *string  `json:"transcoding_location"`
+	BDPMPinChecksEnabled        string   `json:"bdpm_pin_checks_enabled"`
+	BDPMScanQuarantineEnabled   string   `json:"bdpm_scan_quarantine_enabled"`
+	UseRelayCandidatesOnly      bool     `json:"use_relay_candidates_only"`
+	SNMPNetworkManagementSystem *string  `json:"snmp_network_management_system"`
+	HTTPProxy                   *string  `json:"http_proxy"`
+	TURNServer                  *string  `json:"turn_server"`
+	STUNServer                  *string  `json:"stun_server"`
+	ClientTURNServers           []string `json:"client_turn_servers"`
+	ClientSTUNServers           []string `json:"client_stun_servers"`
+	PolicyServer                *string  `json:"policy_server"`
+	EventSinks                  []string `json:"event_sinks"`
+	LiveCaptionsDialOut1        *string  `json:"live_captions_dial_out_1"`
+	LiveCaptionsDialOut2        *string  `json:"live_captions_dial_out_2"`
+	LiveCaptionsDialOut3        *string  `json:"live_captions_dial_out_3"`
 }
 
 // SystemLocationListResponse represents the response from listing system locations

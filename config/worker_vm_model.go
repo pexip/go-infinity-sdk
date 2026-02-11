ed0626ae97bd75cb63b8435e47fc8614b2c2df5b
Mike Assel <mike.assel@pexip.com>
2026-02-11 08:32:52 -0500

    fix worker_vm: update WorkerVM struct to use StaticRoute type for static_routes field

--- config/worker_vm_model.go
+++ config/worker_vm_model.go
@@ -8,49 +8,49 @@ package config
 
 // WorkerVM represents a worker VM configuration
 type WorkerVM struct {
-	ID                         int      `json:"id,omitempty"`
-	Name                       string   `json:"name"`
-	Description                string   `json:"description,omitempty"`
-	Hostname                   string   `json:"hostname"`
-	Domain                     string   `json:"domain"`
-	Address                    string   `json:"address"`
-	Netmask                    string   `json:"netmask"`
-	Gateway                    string   `json:"gateway"`
-	VMCPUCount                 int      `json:"vm_cpu_count,omitempty"`
-	VMSystemMemory             int      `json:"vm_system_memory,omitempty"`
-	DeploymentType             string   `json:"deployment_type,omitempty"`
-	NodeType                   string   `json:"node_type,omitempty"`
-	Transcoding                bool     `json:"transcoding,omitempty"`
-	CloudBursting              bool     `json:"cloud_bursting,omitempty"`
-	Password                   string   `json:"password,omitempty"`
-	MaintenanceMode            bool     `json:"maintenance_mode,omitempty"`
-	MaintenanceModeReason      string   `json:"maintenance_mode_reason,omitempty"`
-	SystemLocation             string   `json:"system_location"`
-	AlternativeFQDN            string   `json:"alternative_fqdn,omitempty"`
-	EnableDistributedDatabase  bool     `json:"enable_distributed_database,omitempty"`
-	EnableSSH                  string   `json:"enable_ssh,omitempty"`
-	IPv6Address                *string  `json:"ipv6_address,omitempty"`
-	IPv6Gateway                *string  `json:"ipv6_gateway,omitempty"`
-	TLSCertificate             *string  `json:"tls_certificate,omitempty"`
-	SecondaryAddress           *string  `json:"secondary_address,omitempty"`
-	SecondaryNetmask           *string  `json:"secondary_netmask,omitempty"`
-	MediaPriorityWeight        *int     `json:"media_priority_weight,omitempty"`
-	SSHAuthorizedKeys          []string `json:"ssh_authorized_keys,omitempty"`
-	SSHAuthorizedKeysUseCloud  bool     `json:"ssh_authorized_keys_use_cloud,omitempty"`
-	StaticNATAddress           *string  `json:"static_nat_address,omitempty"`
-	StaticRoutes               []string `json:"static_routes,omitempty"`
-	SNMPAuthenticationPassword string   `json:"snmp_authentication_password,omitempty"`
-	SNMPCommunity              string   `json:"snmp_community,omitempty"`
-	SNMPMode                   string   `json:"snmp_mode,omitempty"`
-	SNMPPrivacyPassword        string   `json:"snmp_privacy_password,omitempty"`
-	SNMPSystemContact          string   `json:"snmp_system_contact,omitempty"`
-	SNMPSystemLocation         string   `json:"snmp_system_location,omitempty"`
-	SNMPUsername               string   `json:"snmp_username,omitempty"`
-	ServiceManager             bool     `json:"service_manager,omitempty"`
-	ServicePolicy              bool     `json:"service_policy,omitempty"`
-	Signalling                 bool     `json:"signalling,omitempty"`
-	Managed                    bool     `json:"managed,omitempty"`
-	ResourceURI                string   `json:"resource_uri,omitempty"`
+	ID                         int           `json:"id,omitempty"`
+	Name                       string        `json:"name"`
+	Description                string        `json:"description,omitempty"`
+	Hostname                   string        `json:"hostname"`
+	Domain                     string        `json:"domain"`
+	Address                    string        `json:"address"`
+	Netmask                    string        `json:"netmask"`
+	Gateway                    string        `json:"gateway"`
+	VMCPUCount                 int           `json:"vm_cpu_count,omitempty"`
+	VMSystemMemory             int           `json:"vm_system_memory,omitempty"`
+	DeploymentType             string        `json:"deployment_type,omitempty"`
+	NodeType                   string        `json:"node_type,omitempty"`
+	Transcoding                bool          `json:"transcoding,omitempty"`
+	CloudBursting              bool          `json:"cloud_bursting,omitempty"`
+	Password                   string        `json:"password,omitempty"`
+	MaintenanceMode            bool          `json:"maintenance_mode,omitempty"`
+	MaintenanceModeReason      string        `json:"maintenance_mode_reason,omitempty"`
+	SystemLocation             string        `json:"system_location"`
+	AlternativeFQDN            string        `json:"alternative_fqdn,omitempty"`
+	EnableDistributedDatabase  bool          `json:"enable_distributed_database,omitempty"`
+	EnableSSH                  string        `json:"enable_ssh,omitempty"`
+	IPv6Address                *string       `json:"ipv6_address,omitempty"`
+	IPv6Gateway                *string       `json:"ipv6_gateway,omitempty"`
+	TLSCertificate             *string       `json:"tls_certificate,omitempty"`
+	SecondaryAddress           *string       `json:"secondary_address,omitempty"`
+	SecondaryNetmask           *string       `json:"secondary_netmask,omitempty"`
+	MediaPriorityWeight        *int          `json:"media_priority_weight,omitempty"`
+	SSHAuthorizedKeys          []string      `json:"ssh_authorized_keys,omitempty"`
+	SSHAuthorizedKeysUseCloud  bool          `json:"ssh_authorized_keys_use_cloud,omitempty"`
+	StaticNATAddress           *string       `json:"static_nat_address,omitempty"`
+	StaticRoutes               []StaticRoute `json:"static_routes,omitempty"`
+	SNMPAuthenticationPassword string        `json:"snmp_authentication_password,omitempty"`
+	SNMPCommunity              string        `json:"snmp_community,omitempty"`
+	SNMPMode                   string        `json:"snmp_mode,omitempty"`
+	SNMPPrivacyPassword        string        `json:"snmp_privacy_password,omitempty"`
+	SNMPSystemContact          string        `json:"snmp_system_contact,omitempty"`
+	SNMPSystemLocation         string        `json:"snmp_system_location,omitempty"`
+	SNMPUsername               string        `json:"snmp_username,omitempty"`
+	ServiceManager             bool          `json:"service_manager,omitempty"`
+	ServicePolicy              bool          `json:"service_policy,omitempty"`
+	Signalling                 bool          `json:"signalling,omitempty"`
+	Managed                    bool          `json:"managed,omitempty"`
+	ResourceURI                string        `json:"resource_uri,omitempty"`
 }
 
 // WorkerVMCreateRequest represents a request to create a worker VM
@@ -103,7 +103,7 @@ type WorkerVMCreateRequest struct {
 type WorkerVMUpdateRequest struct {
 	ID                         int      `json:"id,omitempty"`
 	Name                       string   `json:"name,omitempty"`
-	Description                string   `json:"description,omitempty"`
+	Description                string   `json:"description"`
 	Hostname                   string   `json:"hostname,omitempty"`
 	Domain                     string   `json:"domain,omitempty"`
 	Address                    string   `json:"address,omitempty"`

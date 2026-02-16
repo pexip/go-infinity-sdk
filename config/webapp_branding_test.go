a53b297a8b78b3fc031f5ebca92891e6c4fd8780
Mike Assel <mike.assel@pexip.com>
2026-02-16 14:11:42 +0100

    fix: continue test improvements (#54)
    * fix azure_tenant_model: update description field in AzureTenantUpdateRequest to be required
    
    * fix sip_proxy_model: make description field required in SIPProxyUpdateRequest
    
    * fix authentication_update_request: make all fields required in AuthenticationUpdateRequest
    
    * fix webapp_branding_test: format Body field in expectedResponse for consistency

--- config/webapp_branding_test.go
+++ config/webapp_branding_test.go
@@ -135,7 +135,7 @@ func TestService_CreateWebappBranding(t *testing.T) {
 	}
 
 	expectedResponse := &types.PostResponseWithUUID{
-		Body: []byte{},
+		Body:         []byte{},
 		ResourceUUID: "123e4567-e89b-12d3-a456-426614174002",
 	}
 

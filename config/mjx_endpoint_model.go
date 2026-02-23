75eb2d09a44a824f57feeb200f64fdc96ea60b3d
Mike Assel <mike.assel@pexip.com>
2026-02-23 09:47:40 -0500

    fix mjx_endpoint_update_request: make description, endpoint_type, mjx_endpoint_group, api_port, api_username, api_password, use_https, verify_cert, poly_username, poly_password, poly_raise_alarms_for_this_endpoint, and webex_device_id fields required in MjxEndpointUpdateRequest

--- config/mjx_endpoint_model.go
+++ config/mjx_endpoint_model.go
@@ -49,20 +49,20 @@ type MjxEndpointCreateRequest struct {
 // MjxEndpointUpdateRequest represents a request to update a MJX endpoint
 type MjxEndpointUpdateRequest struct {
 	Name                           string  `json:"name,omitempty"`
-	Description                    string  `json:"description,omitempty"`
-	EndpointType                   string  `json:"endpoint_type,omitempty"`
+	Description                    string  `json:"description"`
+	EndpointType                   string  `json:"endpoint_type"`
 	RoomResourceEmail              string  `json:"room_resource_email,omitempty"`
-	MjxEndpointGroup               *string `json:"mjx_endpoint_group,omitempty"`
+	MjxEndpointGroup               *string `json:"mjx_endpoint_group"`
 	APIAddress                     *string `json:"api_address,omitempty"`
-	APIPort                        *int    `json:"api_port,omitempty"`
-	APIUsername                    *string `json:"api_username,omitempty"`
-	APIPassword                    *string `json:"api_password,omitempty"`
-	UseHTTPS                       string  `json:"use_https,omitempty"`
-	VerifyCert                     string  `json:"verify_cert,omitempty"`
-	PolyUsername                   *string `json:"poly_username,omitempty"`
-	PolyPassword                   *string `json:"poly_password,omitempty"`
-	PolyRaiseAlarmsForThisEndpoint *bool   `json:"poly_raise_alarms_for_this_endpoint,omitempty"`
-	WebexDeviceID                  *string `json:"webex_device_id,omitempty"`
+	APIPort                        *int    `json:"api_port"`
+	APIUsername                    *string `json:"api_username"`
+	APIPassword                    *string `json:"api_password"`
+	UseHTTPS                       string  `json:"use_https"`
+	VerifyCert                     string  `json:"verify_cert"`
+	PolyUsername                   *string `json:"poly_username"`
+	PolyPassword                   *string `json:"poly_password"`
+	PolyRaiseAlarmsForThisEndpoint *bool   `json:"poly_raise_alarms_for_this_endpoint"`
+	WebexDeviceID                  *string `json:"webex_device_id"`
 }
 
 // MjxEndpointListResponse represents the response from listing MJX endpoints

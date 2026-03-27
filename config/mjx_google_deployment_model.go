5644e72db88e5c4a48d1377db135c086d9452110
Mike Assel <mike.assel@pexip.com>
2026-03-27 11:01:35 -0400

    refactor: update MjxGoogleDeploymentUpdateRequest to make description, client_id, redirect_uri, and refresh_token fields mandatory

--- config/mjx_google_deployment_model.go
+++ config/mjx_google_deployment_model.go
@@ -47,16 +47,16 @@ type MjxGoogleDeploymentCreateRequest struct {
 // MjxGoogleDeploymentUpdateRequest represents a request to update a MJX Google deployment
 type MjxGoogleDeploymentUpdateRequest struct {
 	Name                       string    `json:"name,omitempty"`
-	Description                string    `json:"description,omitempty"`
+	Description                string    `json:"description"`
 	ClientEmail                string    `json:"client_email,omitempty"`
-	ClientID                   string    `json:"client_id,omitempty"`
+	ClientID                   string    `json:"client_id"`
 	ClientSecret               string    `json:"client_secret,omitempty"`
 	PrivateKey                 string    `json:"private_key,omitempty"`
 	UseUserConsent             *bool     `json:"use_user_consent,omitempty"`
 	AuthEndpoint               string    `json:"auth_endpoint,omitempty"`
 	TokenEndpoint              string    `json:"token_endpoint,omitempty"`
-	RedirectURI                string    `json:"redirect_uri,omitempty"`
-	RefreshToken               string    `json:"refresh_token,omitempty"`
+	RedirectURI                string    `json:"redirect_uri"`
+	RefreshToken               string    `json:"refresh_token"`
 	OAuthState                 *string   `json:"oauth_state,omitempty"`
 	MaximumNumberOfAPIRequests int       `json:"maximum_number_of_api_requests,omitempty"`
 	MjxIntegrations            *[]string `json:"mjx_integrations,omitempty"`

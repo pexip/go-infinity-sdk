8e5baccfb25f0880104eb5179c90a3b5afb23eda
Mike Assel <mike.assel@pexip.com>
2026-09-17 17:09:42 -0400

    fix(config): reorder fields in MediaProcessingServer struct for consistency

--- config/media_processing_server_model.go
+++ config/media_processing_server_model.go
@@ -11,8 +11,9 @@ type MediaProcessingServer struct {
 	ID           int    `json:"id,omitempty"`
 	FQDN         string `json:"fqdn"`
 	AppID        string `json:"app_id,omitempty"`
-	PublicJWTKey string `json:"public_jwt_key"`
-	ResourceURI  string `json:"resource_uri,omitempty"`
+	PublicJWTKey            string `json:"public_jwt_key"`
+	RegistrationPrivateKey  string `json:"registration_private_key,omitempty"`
+	ResourceURI             string `json:"resource_uri,omitempty"`
 }
 
 // MediaProcessingServerCreateRequest represents a request to create a media processing server
